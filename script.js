3882495294b63d4f0f5662a2504ea013188a27ab
cruemartin <crue.c.martin@wmich.edu>
2022-05-02 16:50:15 -0400

    end of day

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 const nav_links = document.querySelectorAll("nav ul li a");
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll("section");
 
-
-function changeActive(id){
-    let links = document.querySelectorAll("nav ul li a");
-
-    links.forEach((link) => link.classList.remove("navActive"));
-
-    id.classList.add('navActive');
-
+function changeActive(id) {
+  nav_links.forEach((link) => link.classList.remove("navActive"));
+  id.classList.add("navActive");
 }
 
-function changeActiveNav(){
-    console.log("inchange")
-    let index = sections.length;
+function changeActiveNav() {
+  console.log("inchange");
+  let index = sections.length;
 
-    while(--index && window.scrollY + 50 < sections[index].offsetTop){} 
+  while (--index && window.scrollY < sections[index].offsetTop) {}
 
-    nav_links.forEach((link) => link.classList.remove('navActive'));
-    nav_links[index].classList.add('navActive');
-
+  nav_links.forEach((link) => link.classList.remove("navActive"));
+  nav_links[index].classList.add("navActive");
 }
 
 changeActiveNav();
-window.addEventListener('scroll', changeActiveNav);
+window.addEventListener("scroll", changeActiveNav);
