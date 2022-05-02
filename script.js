3bb0a41060f461dc431cb001c09587a6a7ff1cc2
cruemartin <crue.c.martin@wmich.edu>
2022-05-02 15:01:43 -0400

    added active scrolling in JS

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const nav_links = document.querySelectorAll("nav ul li a");
+const sections = document.querySelectorAll('section');
+
+
 function changeActive(id){
     let links = document.querySelectorAll("nav ul li a");
 
@@ -6,3 +10,17 @@ function changeActive(id){
     id.classList.add('navActive');
 
 }
+
+function changeActiveNav(){
+    console.log("inchange")
+    let index = sections.length;
+
+    while(--index && window.scrollY + 50 < sections[index].offsetTop){} 
+
+    nav_links.forEach((link) => link.classList.remove('navActive'));
+    nav_links[index].classList.add('navActive');
+
+}
+
+changeActiveNav();
+window.addEventListener('scroll', changeActiveNav);
